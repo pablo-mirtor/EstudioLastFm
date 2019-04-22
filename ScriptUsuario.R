3446d4d55fea66e17b61838ab025f8d8d1735625
pablomirtor <pablo.mirtor@gmail.com>
2019-04-23 01:40:00 +0200

    mejoras

--- ScriptUsuario.R
+++ ScriptUsuario.R
@@ -2,33 +2,41 @@
 #Buscar a un usuario de semilla 
 
 apiKey<-"&api_key=82b23813669e5d6580f8a13e66e258e3"
-enlaceUsuario<-"http://ws.audioscrobbler.com/2.0/?method=user.getfriends&user="
+enlaceUsuario<-"http://ws.audioscrobbler.com/2.0/?method=user.getfriends&limit=500&user="
 listaUsuarios<-c('pabloskink')
-maximo<-1000
+maximo<-100
 usuarioMaximo<-''
 maxAmigos<-0
 
 if (!require(XML)) install.packages("XML")
 require(XML)
 
-count<-0
+count<-1
 
 while(length(listaUsuarios) < maximo){
   usuario<-listaUsuarios[count]
+  print(paste("Analizando al usuario",usuario))
   enlace<-paste(enlaceUsuario,usuario,apiKey,sep="")
   datos<-xmlParse(enlace)
   xml_data <- xmlToList(datos)
+  print(xml_data[["friends"]][[".attrs"]])
   usuarios <- as.list(xml_data[["friends"]])
-  
-  if(length(usuarios) > maxAmigos){
-      maxAmigos<-length(usuarios)
+  amigos<-length(usuarios) - 1
+  if(amigos > 0){
+    if(amigos > maxAmigos){
+      maxAmigos<-amigos
       usuarioMaximo<-usuario
-  }
-  
-  for(i in 0:length(usuarios)){
-    if(!is.element(usuarios[[i]][[1]], listaUsuarios)){
-      append(listaUsuarios,usuarios[[i]][[1]])
+    }
+    
+    for(i in 1:amigos){
+      if(!is.element(usuarios[[i]][[1]], listaUsuarios)){
+        listaUsuarios<-c(listaUsuarios,usuarios[[i]][[1]])
+      }
     }
   }
+  
   count<-count+1
 }
+print(listaUsuarios)
+print(usuarioMaximo)
+print(maxAmigos)
