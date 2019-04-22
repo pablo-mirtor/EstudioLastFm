38aceb31a073000c8266bdafebb2f95e1d98ed7a
pablomirtor <pablo.mirtor@gmail.com>
2019-04-22 20:52:45 +0200

    cambios

--- ScriptUsuario.R
+++ ScriptUsuario.R
@@ -1,6 +1,34 @@
 #Script1
+#Buscar a un usuario de semilla 
 
+apiKey<-"&api_key=82b23813669e5d6580f8a13e66e258e3"
+enlaceUsuario<-"http://ws.audioscrobbler.com/2.0/?method=user.getfriends&user="
+listaUsuarios<-c('pabloskink')
+maximo<-1000
+usuarioMaximo<-''
+maxAmigos<-0
 
-apiKey<-"82b23813669e5d6580f8a13e66e258e3"
+if (!require(XML)) install.packages("XML")
+require(XML)
 
-enlace<-"last"
+count<-0
+
+while(length(listaUsuarios) < maximo){
+  usuario<-listaUsuarios[count]
+  enlace<-paste(enlaceUsuario,usuario,apiKey,sep="")
+  datos<-xmlParse(enlace)
+  xml_data <- xmlToList(datos)
+  usuarios <- as.list(xml_data[["friends"]])
+  
+  if(length(usuarios) > maxAmigos){
+      maxAmigos<-length(usuarios)
+      usuarioMaximo<-usuario
+  }
+  
+  for(i in 0:length(usuarios)){
+    if(!is.element(usuarios[[i]][[1]], listaUsuarios)){
+      append(listaUsuarios,usuarios[[i]][[1]])
+    }
+  }
+  count<-count+1
+}
